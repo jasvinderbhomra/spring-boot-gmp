70559b00117a1fd35d6d8e69b1ce9cc59ff7a5af
jbhomra <jasvinderbhomra@gmail.com>
2025-07-10 19:26:41 -0500

    Refactor SampleController to change hello endpoint from GET to POST and update request handling

--- gmp-promet_v1/src/main/java/com/gmp/boot/SampleController.java
+++ gmp-promet_v1/src/main/java/com/gmp/boot/SampleController.java
@@ -4,7 +4,8 @@ import io.micrometer.core.instrument.MeterRegistry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
@@ -22,24 +23,22 @@ class SampleController {
 
     @PostMapping("/hello")
     public String hello(@RequestBody Request requestBody) {
-        if (requestBody.getrLCC() != null) {
-            for (String c : requestBody.getrLCC()) {
-                registry.counter("custom.requested_lcc_total", "lcc", c).increment();
-            }
-        }
-        //registry.counter("custom.hello.count", "status", "ok").increment();
-    @GetMapping("/hello")
-    public String hello() {
+
         try {
-        // Simulate setting request context
-        MDC.put("requestId", UUID.randomUUID().toString());  // Normally from header or filter
-        MDC.put("userId", "jasvinder");                      // Normally from authentication
+            // Simulate setting request context
+            MDC.put("requestId", UUID.randomUUID().toString());       // Normally from header or filter
+            MDC.put("userId", "jasvinder");                      // Normally from authentication
 
-        registry.counter("custom.hello.count", "status", "ok").increment();
+            if (requestBody.getrLCC() != null) {
+                for (String c : requestBody.getrLCC()) {
+                    registry.counter("custom.requested_lcc_total", "lcc", c).increment();
+                }
+            }
 
-        logger.info("Handling /hello request");
+            logger.info("Handling /hello request");
 
-        return "Hello, world!";
+            return "Hello, world!";
+
         } finally {
             MDC.clear(); // Always clear to avoid memory leaks
         }
